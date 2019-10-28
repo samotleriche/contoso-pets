9443d74b7e580b735d4bff427e18736442b07752
Tomas Leriche <795379@cognizant.com>
2019-10-28 15:47:54 -0500

    added size back

--- Models/Product.cs
+++ Models/Product.cs
@@ -11,6 +11,8 @@ namespace ContosoPets.Api.Models
 
         [Required]
         [Range(minimum: 0.01, maximum: (double) decimal.MaxValue)]
-        public decimal Price { get; set; }        
+        public decimal Price { get; set; }   
+
+        public string Size { get; set; }     
     }
 }
