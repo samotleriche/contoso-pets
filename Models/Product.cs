a8820a97848a45dd297875dad47e02d6cab0e5b6
Tomas Leriche <795379@cognizant.com>
2019-10-28 16:52:57 -0500

    added white space

--- Models/Product.cs
+++ Models/Product.cs
@@ -13,6 +13,8 @@ namespace ContosoPets.Api.Models
         [Range(minimum: 0.01, maximum: (double) decimal.MaxValue)]
         public decimal Price { get; set; }   
 
-        public string Size { get; set; }     
+        public string Size { get; set; }  
+        
+           
     }
 }
